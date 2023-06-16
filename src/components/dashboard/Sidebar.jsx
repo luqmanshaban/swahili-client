91a499618817a3901846132b5acf4fbb48753211
Luqman Shaban <luqmanforeal@gmail.com>
2023-06-16 10:12:04 +0300

    making the dashboard responsive

--- src/components/dashboard/Sidebar.jsx
+++ src/components/dashboard/Sidebar.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Sidebar.module.scss'
 import userImage from '../../assets/avatar.jpeg'
 import { useNavigate } from 'react-router-dom'
 
 const Sidebar = ({toggleOrderComponent, toggleHistoryComponent, toggleDiscountComponent, toggleAccountComponent}) => {
+
+  const [active, setActive] = useState(false)
 
   const navigate = useNavigate()
   const Logout = () => {
@@ -11,25 +13,38 @@ const Sidebar = ({toggleOrderComponent, toggleHistoryComponent, toggleDiscountCo
     navigate('/login')
   }
 
+  const toggleActive = () => {
+    setActive(!active)
+  }
+
+  const unToggleActive = () => {
+    setActive(false)
+  }
+
   return (
     <aside className={styles.aside}>
+      <ul className={`${styles.hamburger} ${active ? styles.active : ''}`} onClick={toggleActive}>
+        <span className={styles.bar}></span>
+        <span className={styles.bar}></span>
+        <span className={styles.bar}></span>
+      </ul>
       <h1>Swahili Plate</h1>
-      <nav>
+      <nav className={`${active ? styles.active : ''}`}>
         <h6>Swahili Plate</h6>
-        <ul>
-          <li>
+        <ul className={styles.navMenu}>
+          <li onClick={unToggleActive}>
             <button onClick={toggleOrderComponent} className={styles.btn}>ORDERS</button>
           </li>
-          <li>
+          <li onClick={unToggleActive}>
             <button onClick={toggleHistoryComponent} className={styles.btn}>HISTORY</button>
           </li>
-          <li>
+          <li onClick={unToggleActive}>
             <button onClick={toggleDiscountComponent} className={styles.btn}>DISCOUNT</button>
           </li>
-          <li>
+          <li onClick={unToggleActive}>
             <button onClick={toggleAccountComponent} className={styles.btn}>ACCOUNT</button>
           </li>
-          <li>
+          <li onClick={unToggleActive}>
             <button onClick={toggleAccountComponent} id={styles.accImg}>
               <img src={userImage} alt="img" height={50} />
             </button>
@@ -37,7 +52,7 @@ const Sidebar = ({toggleOrderComponent, toggleHistoryComponent, toggleDiscountCo
         </ul>
         <button onClick={Logout} id={styles.logout}>LOGOUT</button>
       </nav>
-
+         
     </aside>
   )
 }
